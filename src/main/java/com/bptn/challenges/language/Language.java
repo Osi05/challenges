a5ec19b1f7472da4256b05781c25db55408bf662
Callistus <c.o.enekhaze@gmail.com>
2025-03-19 21:07:23 -0400

    created Sino class

--- src/main/java/com/bptn/challenges/language/Language.java
+++ src/main/java/com/bptn/challenges/language/Language.java
@@ -1,36 +1,38 @@
 package com.bptn.challenges.language;
 
 public class Language {
-	
-	
-	//instances for Language
+
+	// instances for Language
 	protected String name;
 	protected int speakersPopulation;
 	protected String regionsSpoken;
 	protected String wordOrder;
-	
-	//constructor for Language
+
+	// constructor for Language
 	public Language(String name, int speakersPopulation, String regionsSpoken, String wordOrder) {
 		this.name = name;
 		this.speakersPopulation = speakersPopulation;
 		this.regionsSpoken = regionsSpoken;
 		this.wordOrder = wordOrder;
-		
+
 	}
-	
-	//creating getInfo method
+
+	// creating getInfo method
 	public void getInfo() {
-		System.out.println(name + " is spoken by " + speakersPopulation + " people mainly in " + regionsSpoken + ".\nThe language follows the word order: " + wordOrder + ".");
+		System.out.println(name + " is spoken by " + speakersPopulation + " people mainly in " + regionsSpoken
+				+ ".\nThe language follows the word order: " + wordOrder + ".\n");
 	}
-	
-	
-	
+
 	public static void main(String[] args) {
-		
-		Language myLang = new Language("Nyu", 100000, "Asia", "verb-object-subject" );
-		
+
+		Language myLang = new Language("Nyu", 100000, "Asia", "verb-object-subject");
+
 		myLang.getInfo();
-		
+
+		Mayan mayanLang = new Mayan("Ki'che'", 2330000);
+
+		mayanLang.getInfo();
+
 	}
 
 }
--- src/main/java/com/bptn/challenges/language/Mayan.java
+++ src/main/java/com/bptn/challenges/language/Mayan.java
@@ -1,5 +1,16 @@
 package com.bptn.challenges.language;
 
-public class Mayan {
+public class Mayan extends Language {
+
+	public Mayan(String name, int speakersPopulation) {
+		super(name, speakersPopulation, "Central America", "verb-object-subject");
+	}
+
+	// Overriding getInfo()
+	public void getInfo() {
+		System.out.println(name + " is spoken by " + speakersPopulation + " people mainly in " + regionsSpoken
+				+ ".\nThe language follows the word order: " + wordOrder + "\nFun fact: " + this.name
+				+ " is an ergative language.\n");
+	}
 
 }
