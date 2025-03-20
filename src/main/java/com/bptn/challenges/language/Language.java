7448ab3b582a331130e34fc89d41cc0c345d3033
Callistus <c.o.enekhaze@gmail.com>
2025-03-19 20:46:57 -0400

    working on Language class

---
+++ src/main/java/com/bptn/challenges/language/Language.java
@@ -0,0 +1,29 @@
+package com.bptn.challenges.language;
+
+public class Language {
+	
+	
+	//instances for Language
+	protected String name;
+	protected int speakersPopulation;
+	protected String regionsSpoken;
+	protected String wordOrder;
+	
+	//constructor for Language
+	public Language(String name, int speakersPopulation, String regionsSpoken, String wordOrder) {
+		this.name = name;
+		this.speakersPopulation = speakersPopulation;
+		this.regionsSpoken = regionsSpoken;
+		this.wordOrder = wordOrder;
+		
+	}
+	
+	//creating getInfo method
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/main/java/com/bptn/challenges/language/Languages.java
+++
@@ -1,5 +0,0 @@
-package com.bptn.challenges.language;
-
-public class Languages {
-
-}
