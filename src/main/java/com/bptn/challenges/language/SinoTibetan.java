7282ecdf36eae502461820e93ce79c4c1dfd3c68
Callistus <c.o.enekhaze@gmail.com>
2025-03-19 21:14:09 -0400

    created Sino object

--- src/main/java/com/bptn/challenges/language/SinoTibetan.java
+++ src/main/java/com/bptn/challenges/language/SinoTibetan.java
@@ -1,5 +1,13 @@
 package com.bptn.challenges.language;
 
-public class SinoTibetan {
-
+public class SinoTibetan extends Language{
+	
+	public SinoTibetan(String name, int speakersPopulation) {
+		
+		super(name, speakersPopulation, "Asia", "subject-object-verb");
+		
+		if(name.contains("Chinese")) {
+			this.wordOrder = "subject-verb-object";
+		}
+	}
 }
