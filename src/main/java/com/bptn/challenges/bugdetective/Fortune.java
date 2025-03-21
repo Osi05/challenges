96fce054fbc738fca443f0eb3e29164f8985d93e
Callistus <c.o.enekhaze@gmail.com>
2025-03-20 21:14:32 -0400

    fortune challenge

--- src/main/java/com/bptn/challenges/bugdetective/Fortune.java
+++ src/main/java/com/bptn/challenges/bugdetective/Fortune.java
@@ -17,6 +17,38 @@ public class Fortune {
 		Fortune fortuneCookie = new Fortune();
 		
 		int fortune = fortuneCookie.generateRandom();
+		
+		if(fortune == 0) {
+			System.out.println();
+		}
+		else if (fortune == 1) {
+			System.out.println();
+		}
+		else if (fortune == 2) {
+			System.out.println();
+		}
+		else if (fortune == 3) {
+			System.out.println();
+		}
+		else if (fortune == 4) {
+			System.out.println();
+		}
+		else if (fortune == 5) {
+			System.out.println();
+		}
+		else if (fortune == 6) {
+			System.out.println();
+		}
+		else if (fortune == 7) {
+			System.out.println();
+		}
+		else if (fortune == 8) {
+			System.out.println("");
+		}
+		else if (fortune == 9) {
+			System.out.println("The fortune you seek is in a different cookie.");
+		}
+		
 	}
 
 }
