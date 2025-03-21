ed7742568fd5edcdfa687f444a46520918e8881a
Callistus <c.o.enekhaze@gmail.com>
2025-03-20 21:20:38 -0400

    fortune challenge completed

--- src/main/java/com/bptn/challenges/bugdetective/Fortune.java
+++ src/main/java/com/bptn/challenges/bugdetective/Fortune.java
@@ -3,52 +3,43 @@ package com.bptn.challenges.bugdetective;
 import java.lang.Math;
 
 public class Fortune {
-	
-	//generateRandom method
+
+	// generateRandom method
 	public int generateRandom() {
-		
-		int rand = (int)(Math.random() * 10);
-		
+
+		int rand = (int) (Math.random() * 10);
+
 		return rand;
 	}
-	
+
 	public static void main(String[] args) {
-		
+
 		Fortune fortuneCookie = new Fortune();
-		
+
 		int fortune = fortuneCookie.generateRandom();
-		
-		if(fortune == 0) {
-			System.out.println();
-		}
-		else if (fortune == 1) {
-			System.out.println();
-		}
-		else if (fortune == 2) {
-			System.out.println();
-		}
-		else if (fortune == 3) {
-			System.out.println();
-		}
-		else if (fortune == 4) {
-			System.out.println();
-		}
-		else if (fortune == 5) {
-			System.out.println();
-		}
-		else if (fortune == 6) {
-			System.out.println();
-		}
-		else if (fortune == 7) {
-			System.out.println();
-		}
-		else if (fortune == 8) {
-			System.out.println("");
-		}
-		else if (fortune == 9) {
+
+		if (fortune == 0) {
+			System.out.println("Flattery will go far tonight.");
+		} else if (fortune == 1) {
+			System.out.println("Don't behave with cold manners.");
+		} else if (fortune == 2) {
+			System.out.println("May you someday be carbon neutral.");
+		} else if (fortune == 3) {
+			System.out.println("You have rice in your teeth.");
+		} else if (fortune == 4) {
+			System.out.println("A conclusion is simply the place where you get tired of thinking.");
+		} else if (fortune == 5) {
+			System.out.println("No snowflake feels rfesponsible in an avalanche.");
+		} else if (fortune == 6) {
+			System.out.println("He who laughs last is laughing at you.");
+		} else if (fortune == 7) {
+			System.out.println("If you look back, you'll soon be going that way.");
+		} else if (fortune == 8) {
+			System.out.println("You will live lone enough to open many fortune cookies.");
+		} else if (fortune == 9) {
 			System.out.println("The fortune you seek is in a different cookie.");
 		}
-		
+
 	}
 
 }
